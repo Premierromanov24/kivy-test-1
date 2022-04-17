5fcc7a5e3cb7e9837676571df8d22fc01ecce3cb
GPeter <87583845+Premierromanov24@users.noreply.github.com>
2022-04-17 09:24:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,32 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.properties import ObjectProperty
 
+
+import socket
+
+HEADER = 64
+PORT = 5055
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "172.29.204.216"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+
+
+
+
 class MyApp(App):
     def build(self):
         return MyGrid()
@@ -17,7 +43,9 @@ class MyGrid(Widget):
     oclass = ObjectProperty(None)
     date = ObjectProperty(None)
     def button1pressed(self):
-        print(self.name.text, self.oclass.text, self.date.text)
+        if str(self.name.text) == DISCONNECT_MESSAGE:
+            send(DISCONNECT_MESSAGE)
+        send(str(self.name.text))
     pass
 
         
